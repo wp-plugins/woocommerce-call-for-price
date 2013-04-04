3a54502a2a92c396234710e816449815e8851ab0
algoritmika <algoritmika@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-04-04 20:27:13 +0000

    Minor bug fixed
    git-svn-id: https://plugins.svn.wordpress.org/woocommerce-call-for-price/tags/1.0.1@691802 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ readme.txt
@@ -0,0 +1,53 @@
+=== WooCommerce Call for Price ===
+Contributors: algoritmika
+Donate link: http://algoritmika.com/donate/
+Tags: woocommerce,call for price
+Requires at least: 3.5.1
+Tested up to: 3.5.1
+Stable tag: 1.0.1
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+This plugin extends the WooCommerce e-commerce plugin by outputting "Call for Price" when price field for product is left empty.
+
+== Description ==
+
+This plugin extends the WooCommerce e-commerce plugin by outputting "Call for Price" when price field for product is left empty.
+
+This plugin was tested with WooCommerce 2.0.5.
+
+= Feedback =
+* We are open to your suggestions and feedback - Thank you for using or trying out one of our plugins!
+* Drop us a line at [www.algoritmika.com](http://www.algoritmika.com)
+
+= More =
+* Vist the [WooCommerce Call for Price plugin page](http://www.algoritmika.com/shop/wordpress-woocommerce-call-for-price-plugin/)
+
+== Installation ==
+
+1. Upload the entire 'woocommerce-call-for-price' folder to the '/wp-content/plugins/' directory
+2. Activate the plugin through the 'Plugins' menu in WordPress
+3. All empty prices will be automatically replaced with "Call for Price".
+
+== Frequently Asked Questions ==
+
+= Can I change "Call for Price" text? =
+
+You need [WooCommerce Call for Price Pro plugin](http://www.algoritmika.com/shop/wordpress-woocommerce-call-for-price-pro-plugin) to change these settings.
+
+== Screenshots ==
+
+1. Empty price replaced by "Call for Price".
+
+== Changelog ==
+
+= 1.0.1 =
+* Minor bug fixed
+
+= 1.0.0 =
+* Initial Release
+
+== Upgrade Notice ==
+
+= 1.0.0 =
+This version was tested with WooCommerce 2.0.5
---
+++ woocommerce-call-for-price.php
@@ -0,0 +1,54 @@
+<?php
+/*
+Plugin Name: WooCommerce Call for Price
+Plugin URI: http://www.algoritmika.com/shop/wordpress-woocommerce-call-for-price-plugin/
+Description: This plugin extends the WooCommerce e-commerce plugin by outputting "Call for Price" when price field for product is left empty.
+Version: 1.0.1
+Author: Algoritmika Ltd.
+Author URI: http://www.algoritmika.com
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+*/
+?>
+<?php
+if ( ! class_exists( 'woocfp_plugin_lite' ) ) {
+	class woocfp_plugin_lite{
+		public function __construct(){
+		
+			add_filter('woocommerce_empty_price_html', array($this, 'empty_price'), 99);
+		
+			//Settings
+			if(is_admin()){
+				add_action('admin_menu', array($this, 'add_plugin_options_page'));
+			}
+		}
+		
+		public function empty_price($price) {
+			return 'Call for Price';
+		}
+		
+		public function add_plugin_options_page(){
+			add_submenu_page( 'woocommerce', 'WooCommerce Call for Price Settings Admin', 'Call for Price Settings', 'manage_options', 'woocfp-lite-settings-admin', array($this, 'create_admin_page'));
+		}
+
+		public function create_admin_page(){
+			?>
+		<div class="wrap">
+			<h2>WooCommerce Call for Price Options</h2>			
+			<form method="post" action="options.php">
+				<div id="message" class="updated fade"><p><strong>*You need <a href='http://www.algoritmika.com/shop/wordpress-woocommerce-call-for-price-pro-plugin/'>'WooCommerce Call for Price Pro'</a> plugin version to change these settings.</strong></p></div>
+				<table class="form-table">
+				<tr valign="top"><th scope="row">Text to output</th><td><input type="text" readonly style="width:300px;" id="woocfp_text_id" name="woocfp_option_group[woocfp_text]" value="Call for Price" /></td></tr>
+				<tr valign="top"><th scope="row">Display on single product</th><td><input disabled type="checkbox" checked id="woocfp_on_single_id" name="woocfp_option_group[woocfp_on_single]" /></td></tr>
+				<tr valign="top"><th scope="row">Display on products archive</th><td><input disabled type="checkbox" checked id="woocfp_on_archive_id" name="woocfp_option_group[woocfp_on_archive]" /></td></tr>
+				<tr valign="top"><th scope="row">Display on home page</th><td><input disabled type="checkbox" checked id="woocfp_on_home_id" name="woocfp_option_group[woocfp_on_home]" /></td></tr>
+				</table>
+				<?php submit_button(); ?>
+			</form>
+		</div>
+		<?php
+		}		
+	}
+}
+
+$woocfp_plugin_lite = &new woocfp_plugin_lite();
